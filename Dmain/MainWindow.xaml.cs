1a792718e3465356dc92e52c4043ee96862e4cc2
Kvitlich <Kvitlich@gmail.com>
2020-01-14 21:41:03 +0600

    Добавьте файлы проекта.

---
+++ Dmain/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dmain
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Uri("SignIn.xaml", UriKind.Relative));
+        }
+
+        private async void Run(object sender, RoutedEventArgs e)
+        {
+
+            using (var client = new SmtpClient("smtp-mail.ru", 465))
+            {
+                client.Credentials = new NetworkCredential("almaapai7@mail.com", "4#74semSum4p%");
+                client.EnableSsl = true;
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("almaapai7@mail.com", "Kvit");
+                message.To.Add(new MailAddress("Kvitlich@gmail.com"));
+                message.Subject = "Die";
+                message.Attachments.Add(new Attachment("1.txt"));
+                message.Body = "Frethin sectum!";
+                await client.SendMailAsync(message);
+            }
+            
+        }
+    }
+}
---
+++ Dmain/SignIn.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dmail
+{
+    /// <summary>
+    /// Логика взаимодействия для SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Page
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            using (var client = new SmtpClient(EmailTextBox.Text, 465))
+            {
+                
+            }
+            this.NavigationService.Navigate(new Uri("PostPage.xaml", UriKind.Relative));
+        }
+    }
+}
